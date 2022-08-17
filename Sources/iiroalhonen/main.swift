13ca4d3068304717fe4dfca6e379f238472bc744
Iiro Alhonen <iiro.alhonen@sanoma.com>
2022-08-17 13:01:45 +0300

    Finish renames of the package.

--- Sources/iiroalhonen/main.swift
+++ Sources/iiroalhonen/main.swift
@@ -3,7 +3,7 @@ import Publish
 import Plot
 
 // This type acts as the configuration for your website.
-struct IikeliGithubIo: Website {
+struct iiroalhonen: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
         case posts
@@ -14,12 +14,12 @@ struct IikeliGithubIo: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://your-website-url.com")!
-    var name = "IikeliGithubIo"
-    var description = "A description of IikeliGithubIo"
+    var url = URL(string: "https://www.iiroalhonen.com")!
+    var name = "iiroalhonen"
+    var description = "A description of iiroalhonen.com"
     var language: Language { .english }
     var imagePath: Path? { nil }
 }
 
 // This will generate your website using the built-in Foundation theme:
-try IikeliGithubIo().publish(withTheme: .foundation)
+try iiroalhonen().publish(withTheme: .foundation)
